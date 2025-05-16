22112f2ae79a024e4408cf60d162cb945874ded5
marcnyamweya@ <marcnyamweya@gmail.com>
2025-05-16 17:21:58 +0300

    updated jest file

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,8 @@
-module.exports = {
+import type { Config } from 'jest';
+
+const config: Config = {
   moduleDirectories: ['node_modules', '.'],
-  setupFilesAfterEnv: ['<rootDir>/.storybook/jest.setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.storybook/jest.setup.ts'],
 };
+
+export default config;
